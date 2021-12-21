33295d0942710204250b194d591945269a2f4271
Seung Bum <wer06099@naver.com>
2021-12-21 17:16:54 +0900

    baekseungbum: 2일차 개발과제

--- app/src/public/js/selectbox.js
+++ app/src/public/js/selectbox.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const select = document.querySelector("select");
+const btn = document.querySelector("button");
+const result = document.getElementById("result");
+
+const submitBtn = () => {
+    const data = {
+      answer: select.value
+    };
+    fetch("/api/selectbox/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "charset": "utf-8"
+      },
+      body: JSON.stringify(data),
+    })
+    .then(res => res.json())
+    .then(answer => (result.innerHTML = answer ? "정답" : "오답"));
+  };
+  
+  btn.addEventListener("click", submitBtn);
