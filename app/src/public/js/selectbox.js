b68433302b2d9403b45b1ac1ae60d1be6a9761f1
leephoter <estarg51@gmail.com>
2021-12-21 14:30:58 +0900

    이한결 제출합니다 (2일차 퍼블리싱)

--- app/src/public/js/selectbox.js
+++ app/src/public/js/selectbox.js
@@ -1,1 +1,28 @@
 "use strict";
+
+const getSelect = document.querySelector("select");
+let answer = "";
+getSelect.addEventListener("change", function (e) {
+    answer = e.target.value;
+});
+const judgeAnswer = {
+    true: "정답",
+    false: "오답",
+};
+document.getElementById("submit-btn").addEventListener("click", function () {
+    fetch("/api/selectbox/submit", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json; charset=utf-8",
+        },
+        body: JSON.stringify({
+            answer,
+        }),
+    })
+        .then((res) => res.json())
+        .then((res) => {
+            console.log(`res`, res);
+            document.getElementById("result").innerText = judgeAnswer[res];
+        });
+    // then 두번 쓰는 이유 >> fetch 공부
+});
