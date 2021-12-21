b94db8f757aa70ec047cc2cf5bba4c8b23fb845c
Lami <Halam.Lee.Lami@gmail.com>
2021-12-21 15:47:19 +0900

    이하람 checkbox 제출합니다.

--- app/src/public/js/checkbox.js
+++ app/src/public/js/checkbox.js
@@ -1,1 +1,38 @@
-"use strict";
+'use strict';
+
+let getChecked = () => {
+  const query = 'input[type="checkbox"]:checked';
+  const selectedEls = document.querySelectorAll(query);
+  let check = [];
+
+  selectedEls.forEach((el) => {
+    check.push(el.value);
+  });
+  return check;
+};
+
+function getResult(check) {
+  const data = {
+    answers: check,
+  };
+  const request = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+    body: JSON.stringify(data),
+  };
+
+  fetch('/api/checkbox/submit', request)
+    .then((res) => res.json())
+    .then((data) => changeResult(data));
+}
+
+function changeResult(data) {
+  document.querySelector('#result').innerHTML = data ? '정답' : '오답';
+}
+
+document.querySelector('#submit_btn').addEventListener('click', () => {
+  const value = getChecked();
+  getResult(value);
+});
