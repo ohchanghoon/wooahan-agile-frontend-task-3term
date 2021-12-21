2889806728ea4093f11a179e05a3db7d0cec88aa
김성제 <rlatjdwp0211@naver.com>
2021-12-21 17:46:50 +0900

    김성제 제출합니다. (2일차)

--- app/src/public/js/checkbox.js
+++ app/src/public/js/checkbox.js
@@ -1,1 +1,25 @@
-"use strict";
+let inputValue = document.querySelectorAll("#checkbox label input");
+let btnValue = document.querySelector("#submit_btn");
+let resultText = document.querySelector("#result");
+
+const onSubmit = () => {
+    const answer = [];
+    
+    for (let i = 0; i < inputValue.length; i++) {
+        if (inputValue[i].checked) answer.push(inputValue[i].value);
+    }
+    fetch('/api/checkbox/submit', {
+        method : 'POST',
+        headers : {
+            'Content-type' : 'application/json; charset=utf-8'
+        },
+        body : JSON.stringify({
+            answers : answer,
+        }),
+    })
+    .then((res) => res.json())
+    .then((res) => {res === true ? resultText.innerText="정답" 
+    : resultText.innerText="오답";});
+}
+
+btnValue.addEventListener('click', () => onSubmit())
