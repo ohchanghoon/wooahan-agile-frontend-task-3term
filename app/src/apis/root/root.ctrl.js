d286047bf8603268f691657dc2650a75dc3a2797
leephoter <estarg51@gmail.com>
2021-12-21 14:37:19 +0900

    이한결 제출합니다 (2일차 퍼블리싱)

--- app/src/apis/root/root.ctrl.js
+++ app/src/apis/root/root.ctrl.js
@@ -1,138 +1,149 @@
-'use strict';
+"use strict";
 
-let db = require('../../config/db');
-const crypto = require('crypto');
+let db = require("../../config/db");
+const crypto = require("crypto");
 
 const todolist = {
-  read: (req, res) => {
-    const query = 'SELECT * FROM todos;';
+    read: (req, res) => {
+        const query = "SELECT * FROM todos;";
 
-    db.query(query, (err, posts) => {
-      if (err) throw err;
-      return res.status(200).send(posts);
-    });
-  },
+        db.query(query, (err, posts) => {
+            if (err) throw err;
+            return res.status(200).send(posts);
+        });
+    },
 
-  create: (req, res) => {
-    const client = req.body;
-    const index = crypto.randomBytes(20).toString('hex').slice(0, 20); // token 생성
+    create: (req, res) => {
+        const client = req.body;
+        const index = crypto.randomBytes(20).toString("hex").slice(0, 20); // token 생성
 
-    const query = 'INSERT INTO todos(id, name, description) VALUES (?, ?, ?);';
+        const query =
+            "INSERT INTO todos(id, name, description) VALUES (?, ?, ?);";
 
-    db.query(query, [index, client.name, client.description], (err, result) => {
-      if (err) throw err;
-      return res.status(201).send(index);
-    });
-  },
+        db.query(
+            query,
+            [index, client.name, client.description],
+            (err, result) => {
+                if (err) throw err;
+                return res.status(201).send(index);
+            }
+        );
+    },
 
-  update: (req, res) => {
-    const index = req.params.index;
-    const client = req.body;
+    update: (req, res) => {
+        const index = req.params.index;
+        const client = req.body;
 
-    const query = 'UPDATE todos SET name=?, description=? WHERE id=?;';
+        const query = "UPDATE todos SET name=?, description=? WHERE id=?;";
 
-    db.query(query, [client.name, client.description, index], (err, result) => {
-      if (err) throw err;
-      if (result.affectedRows) {
-        return res.status(201).send(index);
-      }
-      return res.status(200).send('수정되지 않았습니다.');
-    });
-  },
+        db.query(
+            query,
+            [client.name, client.description, index],
+            (err, result) => {
+                if (err) throw err;
+                if (result.affectedRows) {
+                    return res.status(201).send(index);
+                }
+                return res.status(200).send("수정되지 않았습니다.");
+            }
+        );
+    },
 
-  delete: (req, res) => {
-    const index = req.params.index;
+    delete: (req, res) => {
+        const index = req.params.index;
 
-    const query = 'DELETE FROM todos WHERE id=?;';
+        const query = "DELETE FROM todos WHERE id=?;";
 
-    db.query(query, [index], (err, result) => {
-      if (err) throw err;
-      if (result.affectedRows) {
-        return res.status(201).send(index);
-      }
-      return res.status(200).send('삭제되지 않았습니다.');
-    });
-  },
+        db.query(query, [index], (err, result) => {
+            if (err) throw err;
+            if (result.affectedRows) {
+                return res.status(201).send(index);
+            }
+            return res.status(200).send("삭제되지 않았습니다.");
+        });
+    },
 };
 
 const selectbox = {
-  isAnswer: (req, res) => {
-    const client = req.body;
-    if (client.answer !== '개발') {
-      return res.json(false);
-    }
-    return res.json(true);
-  },
+    isAnswer: (req, res) => {
+        const client = req.body;
+        if (client.answer !== "개발") {
+            return res.json(false);
+        }
+        return res.json(true);
+    },
 };
 
 const checkbox = {
-  isAnswer: (req, res) => {
-    const client = req.body;
-    const answers = ['열정', '기술력', '인성'];
+    isAnswer: (req, res) => {
+        const client = req.body;
+        const answers = ["열정", "기술력", "인성"];
 
-    if (!client.answers || client.answers.length !== 3) {
-      return res.send(false);
-    }
+        if (!client.answers || client.answers.length !== 3) {
+            return res.send(false);
+        }
 
-    let cnt = 0;
-    for (let clnt of client.answers) {
-      if (answers.includes(clnt)) cnt++;
-    }
+        let cnt = 0;
+        for (let clnt of client.answers) {
+            if (answers.includes(clnt)) cnt++;
+        }
 
-    if (cnt === 3) {
-      return res.json(true);
-    }
-    return res.json(false);
-  },
+        if (cnt === 3) {
+            return res.json(true);
+        }
+        return res.json(false);
+    },
 };
 
 const login = {
-  login: (req, res) => {
-    const client = req.body;
-    if (client.id !== 'test' || client.password !== 'test') {
-      return res
-        .status(400)
-        .json({ msg: '아이디와 패스워드를 제대로 입력하십시오' });
-    }
+    login: (req, res) => {
+        const client = req.body;
+        if (client.id !== "test" || client.password !== "test") {
+            return res
+                .status(400)
+                .json({ msg: "아이디와 패스워드를 제대로 입력하십시오" });
+        }
 
-    const query = 'SELECT * FROM users WHERE id=? AND psword=?;';
+        const query = "SELECT * FROM users WHERE id=? AND psword=?;";
 
-    db.query(query, [client.id, client.password], (err, users) => {
-      if (err) throw err;
-      return res.status(200).json({ msg: 'success', code: users[0].code });
-    });
-  },
+        db.query(query, [client.id, client.password], (err, users) => {
+            if (err) throw err;
+            return res
+                .status(200)
+                .json({ msg: "success", code: users[0].code });
+        });
+    },
 
-  check: (req, res) => {
-    const code = req.query.code;
-    if (!code)
-      return res.status(400).json({ msg: '정보확인에 실패하셨습니다.' });
+    check: (req, res) => {
+        const code = req.query.code;
+        if (!code)
+            return res.status(400).json({ msg: "정보확인에 실패하셨습니다." });
 
-    const query = 'SELECT * FROM users WHERE code=?;';
+        const query = "SELECT * FROM users WHERE code=?;";
 
-    db.query(query, [code], (err, users) => {
-      if (err) throw err;
-      const user = users[0];
-      if (user) {
-        const response = {
-          msg: 'success',
-          user: {
-            id: user.id,
-            password: user.psword,
-            name: user.name,
-            code: user.code,
-          },
-        };
-        return res.status(200).json(response);
-      }
-      return res.status(400).json({ msg: '정보확인에 실패하셨습니다.' });
-    });
-  },
+        db.query(query, [code], (err, users) => {
+            if (err) throw err;
+            const user = users[0];
+            if (user) {
+                const response = {
+                    msg: "success",
+                    user: {
+                        id: user.id,
+                        password: user.psword,
+                        name: user.name,
+                        code: user.code,
+                    },
+                };
+                return res.status(200).json(response);
+            }
+            return res.status(400).json({ msg: "정보확인에 실패하셨습니다." });
+        });
+    },
 };
 
 module.exports = {
-  todolist,
-  selectbox,
-  checkbox,
-  login,
+    todolist,
+    selectbox,
+    checkbox,
+    login,
 };
--- app/src/public/js/checkbox.js
+++ app/src/public/js/checkbox.js
@@ -1,1 +1,26 @@
 "use strict";
+
+const getChecked = document.getElementById("checkbox");
+const answers = [];
+getChecked.addEventListener("change", function (e) {
+    answers.push(e.target.value);
+});
+
+const judgeAnswer = {
+    true: "정답",
+    false: "오답",
+};
+
+document.getElementById("submit_btn").addEventListener("click", function () {
+    fetch("/api/checkbox/submit", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json; charset=utf-8",
+        },
+        body: JSON.stringify({ answers }),
+    })
+        .then((res) => res.json())
+        .then((res) => {
+            document.getElementById("result").innerText = judgeAnswer[res];
+        });
+});
