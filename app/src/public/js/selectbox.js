4b41f4452f8feebe8295d142624f42418907bffc
Lee-Mina <Lee-Mina>
2021-12-23 17:33:16 +0900

    이미나 제출합니다.

--- app/src/public/js/selectbox.js
+++ app/src/public/js/selectbox.js
@@ -1,1 +1,27 @@
-"use strict";
+"use strict"
+const button = document.querySelector('#submit-btn');
+const result = document.querySelector('#result');
+const select = document.querySelector('select');
+let value = "";
+
+select.addEventListener('change', () => {
+    value = select.value;
+})
+
+button.addEventListener('click', () => {
+    fetch('/api/selectbox/submit',{
+        method: "POST",
+        headers: {
+            "Content-type" : "application/json; charset=utf-8"
+        },
+        body: JSON.stringify({ answer: value })
+    })
+    .then((res) => res.json())
+    .then((answer) => {
+        if (answer === true) {
+            document.querySelector('#result').innerHTML = '정답'
+        } else {
+            document.querySelector('#result').innerHTML = '오답' 
+        }
+    })
+});
