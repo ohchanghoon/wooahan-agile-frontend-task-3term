ae7afd89b2e1fb93cfb58d5cc4af8caffd227062
Lee-Mina <Lee-Mina>
2021-12-26 15:45:08 +0900

    이미나 제출합니다.

--- app/src/public/js/login.js
+++ app/src/public/js/login.js
@@ -5,24 +5,13 @@ const id = document.querySelector('[for="id"]');
 const password = document.querySelector('[for="password"]');
 let answer = "";
 
-// id.addEventListener(input,() => {
-//     value = input.value;
-// });
-// password.addEventListener(input,() => {
-//     value = input.value;
-// })
-
 button.addEventListener("click", () => {
   //아이디 패스워드 갖고오는 로직
-  //   fetch(`/api/check?code=${}`, {
-  //     method: "GET",
-  //     headers: {
-  //       "Content-type": "application/json; charset=utf-8",
-  //     },
-  //   });
-  //input type = ""
-  const IdValue = document.getElementById("냠냠");
-  const PasswordValue = document.getElementById("쩝쩝");
+
+  // const IdValue = document.getElementById("냠냠");
+  // const PasswordValue = document.getElementById("쩝쩝");
+  const IdValue = "냠냠";
+  const PasswordValue = "쩝쩝";
   fetch("/api/login", {
     method: "POST",
     headers: {
@@ -32,10 +21,12 @@ button.addEventListener("click", () => {
   })
     .then((res) => res.json())
     .then((id, password) => {
-      if (id == IdValue && password == PasswordValue) {
-        alert("정답");
-      } else {
-        alert("오답");
+      if (id == IdValue) {
+        if (password == PasswordValue) {
+          alert("환영합니다.");
+        } else {
+          alert("아이디가 일치하지 않습니다.");
+        }
       }
     });
 });
