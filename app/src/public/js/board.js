d7432b3946248608fd4b5cea87bcb9b21731d2a0
김성제 <rlatjdwp0211@naver.com>
2021-12-27 00:16:46 +0900

    김성제 프론트 과제 제출합니다. (5일차)

--- app/src/public/js/board.js
+++ app/src/public/js/board.js
@@ -1,1 +1,32 @@
 "use strict";
+// let writerHidden = res.boards.writerHiddenFlag;
+const parentTable = document.querySelector("table");
+const noticePostNumDiv = document.querySelector(".noticePostNumDiv");
+
+
+fetch ('https://dongurami.tk/api/board/notice', {
+    method: 'GET',
+})
+    .then((res) => res.json())
+    .then((res) => {
+        for (let i = 0; i < res.boards.length; i ++) {
+            let num = res.boards[i].no;
+            let title = res.boards[i].title;
+            let writer = res.boards[i].studentName;
+            let date = res.boards[i].inDate;
+            let views = res.boards[i].hit;
+            let likes = res.boards[i].emotionCount;
+            const newTr = document.createElement('tr')
+            
+            newTr.innerHTML = `<td>${num}</td>
+                                <td>${title}</td>
+                                <td>${writer}</td>
+                                <td>${date}</td>
+                                <td>${views}</td>
+                                <td>${likes}</td>`
+                                parentTable.appendChild(newTr);
+        
+        }
+        let noticePostNum = res.boards.length;
+        noticePostNumDiv.innerHTML = `총 ${noticePostNum}건의 글이 있습니다.`;
+    })
